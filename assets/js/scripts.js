00edafbcfb5a6249b9bfaceb2f92760e672b7aa1
Aaron <newcomer.aaron@icloud.com>
2016-06-07 16:41:14 -0400

    Add attempt at content

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,4 @@
 $(function () {
-
   $('.searchbar').submit(function (e) {
     e.preventDefault();
     console.log("Submit")
@@ -8,8 +7,8 @@ $(function () {
     $.getJSON('https://api.github.com/users/' + ghLogin)
       .done(showUser);
     // $.getJSON('https://api.github.com/users/'+ghLogin+'/repos')
-    //   .done(showUser2)
-    //   .fail(showError);
+    //    .done(showUser2)
+    //    .fail(showError);
   });
 
   function showUser(user) {
@@ -30,6 +29,46 @@ $(function () {
     var fn = _.template($('#' + template).html(), { variable: 'm' });
     $('.user-info').html(fn(model));
     $('.user-info2').html(fn(model));
+  }
+
+});
+
+
+
+
+$(function () {
+  $('.searchbar').submit(function (e) {
+    e.preventDefault();
+    console.log("Submit")
+
+    var ghLogin = $('input[name="gh-login"]').val();
+    // $.getJSON('https://api.github.com/users/' + ghLogin)
+    //   .done(showUser);
+    $.getJSON('https://api.github.com/users/'+ghLogin+'/repos')
+       .done(showUser2)
+       .fail(showError);
+  });
+
+  // function showUser(user) {
+  //   show('gh-user-template', user);
+  // }
+  function showUser2(user) {
+    console.log("Please")
+    show2('gh-user-template2', user);
+  }
+
+  function showError(req, status, err) {
+    err = err || {};
+    err.message = err.message || status;
+    console.log(err);
+    show2('gh-error-template2', { message: err });
+  }
+
+  function show2(template, model) {
+    console.log("prettyPlease")
+    var fn2 = _.template($('#' + template).html(), { variable: 'm' });
+    // $('.user-info').html(fn(model));
+    $('.user-info2').html(fn2(model));
   }
 
 });
